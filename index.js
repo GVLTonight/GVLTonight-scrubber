67f03cf7d75b1f04e8e6a4a97d7f361d69e278e3
Kevin Bost <kevbost@gmail.com>
2017-07-21 15:48:29 -0400

    added package to append to ./GVLLog.json

--- index.js
+++ index.js
@@ -2,19 +2,34 @@
 const scrubber = require('./lib/main.js');
 const Dossier = require('./dossier');
 const logger = require('./utils/logFile');
+const moment = require('moment');
+const updater = require('jsonfile-updater');
+const fs = require('fs');
 
 async function entry() {
   return Promise.all([
-    scrubber(Dossier.gvltonight.data(), Dossier.gvltonight.collection, Dossier.gvltonight.market),
-    scrubber(Dossier.colatonight.data(), Dossier.colatonight.collection, Dossier.colatonight.market),
-    scrubber(Dossier.avltonight.data(), Dossier.avltonight.collection, Dossier.avltonight.market),
+    scrubber(
+      Dossier.gvltonight.data(),
+      Dossier.gvltonight.collection,
+      Dossier.gvltonight.market),
+    scrubber(
+      Dossier.colatonight.data(),
+      Dossier.colatonight.collection,
+      Dossier.colatonight.market,
+    ),
+    scrubber(
+      Dossier.avltonight.data(),
+      Dossier.avltonight.collection,
+      Dossier.avltonight.market,
+    ),
   ]);
-  // return [r1, r2, r3];
 }
 
 entry().then((log) => {
-  console.log(logger);
-  // console.log(log);
-});
+  const file = 'GVLLog.json';
 
-// console.log(Dossier.gvltonight.test(Dossier.gvltonight));
+  updater(file).add(moment().format(), log[0], (err) => {
+    if (err) return console.log(err);
+    return console.log(log[0]);
+  });
+});
